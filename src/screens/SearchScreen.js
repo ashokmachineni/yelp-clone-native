deb4a3e79e0c1047d3b827b490caceb8544bbc49
ashokmachineni <ashok5679m@gmail.com>
2020-06-22 12:12:34 +0100

    finished data fetching from yelp api with axios

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "london"
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("something pissy");
+    }
+  };
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log("its submited")}
-      />
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>total results : {results.length}</Text>
     </View>
   );
 };
